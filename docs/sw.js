5607c3af1c0db3d54880ec8dde78de367365b915
hagevvashi <hagevvashi@gmail.com>
2020-12-02 23:07:57 +0900

    wip: fetch event

--- docs/sw.js
+++ docs/sw.js
@@ -1,3 +1,9 @@
 self.addEventListener("install", () => {
   console.log("start install");
 });
+
+self.addEventListener("fetch", (event) => {
+  console.log('Handling fetch event for', event.request.url);
+  const requestUrl = new URL(event.request.url);
+  console.log(requestUrl);
+});
